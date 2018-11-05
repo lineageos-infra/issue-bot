5df75e8a341b3168d57314f34d62211147e91b70
Tom Powell <tom@powell.io>
2018-11-05 05:56:07 +0000

    Check for gitlab webhook token

--- bot/app.py
+++ bot/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 import json
 
 from bot.gitlab.webhooks import Issue
@@ -8,6 +8,8 @@ app = Flask(__name__)
 @app.route("/", methods=("POST",))
 @app.route("/webhook", methods=("POST",))
 def webhook():
+    if request.headers.get("X-Gitlab-Token") != config.GITLAB_WEBHOOK_TOKEN:
+        abort(403)
     if request.headers.get("X-Gitlab-Event") == "Issue Hook":
         print('changes' in request.json)
         issue = Issue.from_issue_hook(request.json)
--- bot/config.py
+++ bot/config.py
@@ -1,2 +1,3 @@
 import os
 GITLAB_TOKEN = os.environ.get("GITLAB_TOKEN", None)
+GITLAB_WEBHOOK_TOKEN = os.environ.get("GITLAB_WEBHOOK_TOKEN", None)
