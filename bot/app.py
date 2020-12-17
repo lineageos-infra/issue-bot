3c178143500c7ec601cc23b93c78a891a1cf08b4
Tom Powell <tom@powell.io>
2020-12-16 21:42:15 -0800

    format with black

--- bot/app.py
+++ bot/app.py
@@ -9,8 +9,13 @@ from bot import config
 headers = {"Private-Token": config.GITLAB_TOKEN}
 project = 9202919
 options = {
-    'version': ["lineage-16.0", "lineage-17.1"],
-    'device':  [x["model"] for x in requests.get("https://raw.githubusercontent.com/LineageOS/hudson/master/updater/devices.json").json()]
+    "version": ["lineage-16.0", "lineage-17.1"],
+    "device": [
+        x["model"]
+        for x in requests.get(
+            "https://raw.githubusercontent.com/LineageOS/hudson/master/updater/devices.json"
+        ).json()
+    ],
 }
 
 print(options)
@@ -22,14 +27,13 @@ label_data = {
     },
     "version": {
         "data": True,
-        "error": "- The version of LineageOS running on your device is required (include /version (lineage-16.0,lineage-17.1)."
+        "error": "- The version of LineageOS running on your device is required (include /version (lineage-16.0,lineage-17.1).",
     },
     "date": {
         "data": False,
-        "error": "- Build date is required (include /date YYYY-MM-DD)."
-    }
+        "error": "- Build date is required (include /date YYYY-MM-DD).",
+    },
 }
-
 
 
 def validate(description):
@@ -46,7 +50,9 @@ def validate(description):
                     if value in options[label[1:]]:
                         labels.append(f"{label[1:]}:{value}")
                     elif value:
-                        errors.append(f"- {value} is not a valid {label[1:]}. Supported values are {options[label[1:]]}")
+                        errors.append(
+                            f"- {value} is not a valid {label[1:]}. Supported values are {options[label[1:]]}"
+                        )
                 else:
                     labels.append(f"{label[1:]}")
     missing_labels = label_data.keys() - set(seen)
@@ -54,18 +60,32 @@ def validate(description):
         errors.append(label_data[label]["error"])
     return labels, errors
 
+
 def post_reply(iid, reply):
-    resp = requests.post(f"https://gitlab.com/api/v4/projects/{project}/issues/{iid}/notes", json={"body": "\n".join(reply)}, headers=headers)
+    resp = requests.post(
+        f"https://gitlab.com/api/v4/projects/{project}/issues/{iid}/notes",
+        json={"body": "\n".join(reply)},
+        headers=headers,
+    )
     if resp.status_code != 201:
         print(f"Error replying - ${resp.json()}")
 
+
 def edit_issue(iid, edits):
-    resp = requests.put(f"https://gitlab.com/api/v4/projects/{project}/issues/{iid}", json=edits, headers=headers)
+    resp = requests.put(
+        f"https://gitlab.com/api/v4/projects/{project}/issues/{iid}",
+        json=edits,
+        headers=headers,
+    )
     if resp.status_code != 200:
         print(f"Error updating labels - ${resp.json()}")
 
+
 def process_new():
-    resp = requests.get(f"https://gitlab.com/api/v4/projects/{project}/issues?state=opened&labels=None", headers=headers)
+    resp = requests.get(
+        f"https://gitlab.com/api/v4/projects/{project}/issues?state=opened&labels=None",
+        headers=headers,
+    )
     if resp.status_code != 200:
         print(f"Error getting issues - {resp.json()}")
         return
@@ -74,25 +94,31 @@ def process_new():
         reply = None
         if errors:
             labels.append("invalid")
-            reply = [
-                "Hi! It appears you didn't read or follow the provided issue template. Your issue has been marked as invalid. You can either edit your issue to include the requested fields and reopen it, or create a new issue following the provided template. For more information, please see https://wiki.lineageos.org/bugreport-howto.html",
-                "",
-                "Problems:",
-                ""
-            ] + errors + ["", "(this action was performed by a bot)"]
+            reply = (
+                [
+                    "Hi! It appears you didn't read or follow the provided issue template. Your issue has been marked as invalid. You can either edit your issue to include the requested fields and reopen it, or create a new issue following the provided template. For more information, please see https://wiki.lineageos.org/bugreport-howto.html",
+                    "",
+                    "Problems:",
+                    "",
+                ]
+                + errors
+                + ["", "(this action was performed by a bot)"]
+            )
         if reply:
             post_reply(issue["iid"], reply)
         # edit issue
-        edits = {
-            "labels": ",".join(labels)
-        }
+        edits = {"labels": ",".join(labels)}
         if "invalid" in labels:
             edits["state_event"] = "close"
         edit_issue(issue["iid"], edits)
         print(f"new: {issue['web_url']}")
 
+
 def process_invalid():
-    resp = requests.get(f"https://gitlab.com/api/v4/projects/{project}/issues?state=opened&labels=invalid", headers=headers)
+    resp = requests.get(
+        f"https://gitlab.com/api/v4/projects/{project}/issues?state=opened&labels=invalid",
+        headers=headers,
+    )
     if resp.status_code != 200:
         print(f"Error getting invalid issues - {resp.json()}")
         return
@@ -101,25 +127,27 @@ def process_invalid():
         reply = None
         if errors:
             labels.append("invalid")
-            reply = [
-                "Hi! It appears this issue still has problems - please fix the things below and reopen it!",
-                "",
-                "Problems:",
-                ""
-            ] + errors + ["", "(this action was performed by a bot)"]
+            reply = (
+                [
+                    "Hi! It appears this issue still has problems - please fix the things below and reopen it!",
+                    "",
+                    "Problems:",
+                    "",
+                ]
+                + errors
+                + ["", "(this action was performed by a bot)"]
+            )
         if reply:
             post_reply(issue["iid"], reply)
-        edits = {
-            "labels": ",".join(labels)
-        }
+        edits = {"labels": ",".join(labels)}
         if "invalid" in labels:
             edits["state_event"] = "close"
         edit_issue(issue["iid"], edits)
         print(f"invalid: {issue['web_url']}")
+
 
 if __name__ == "__main__":
     while True:
         process_new()
         process_invalid()
         time.sleep(60)
-
--- test.py
+++ test.py
@@ -8,9 +8,11 @@ from bot.app import app
 
 from bot.gitlab.webhooks import Issue
 
+
 class UsesApp(flask_testing.TestCase):
     def create_app(self):
         return app
+
 
 class TestGitlabWebhooksIssue(unittest.TestCase):
     def test_invalid_from_issue(self):
@@ -18,24 +20,30 @@ class TestGitlabWebhooksIssue(unittest.TestCase):
             data = json.loads(f.read())
 
         issue = Issue.from_issue_hook(data)
-        assert issue.validated_labels == ['device:mako', 'invalid']
+        assert issue.validated_labels == ["device:mako", "invalid"]
         assert len(issue.errors) > 0
 
     def test_valid(self):
 
         data = {
-            "user": {
-                "username": "banana"
-            },
+            "user": {"username": "banana"},
             "object_attributes": {
                 "id": 1,
-                "description": "/device mako\\n/version lineage-17.1\\n/date 2018-01-01\\n/kernel 3\\n/mods None\\n/baseband 4"
-            }
+                "description": "/device mako\\n/version lineage-17.1\\n/date 2018-01-01\\n/kernel 3\\n/mods None\\n/baseband 4",
+            },
         }
 
         issue = Issue.from_issue_hook(data)
-        assert issue.validated_labels == ['device:mako', 'version:lineage-17.1', 'date', 'kernel', 'mods', 'baseband']
+        assert issue.validated_labels == [
+            "device:mako",
+            "version:lineage-17.1",
+            "date",
+            "kernel",
+            "mods",
+            "baseband",
+        ]
         assert issue.errors == []
+
 
 if __name__ == "__main__":
     unittest.main()
